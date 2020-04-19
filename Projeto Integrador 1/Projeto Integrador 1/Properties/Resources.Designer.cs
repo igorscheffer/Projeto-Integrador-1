ec4832be5900826cc0c4e7aab7b4c8a296d5fa34
Igor Scheffer Ferreira <igorschefferferreira@hotmail.com.br>
2020-04-19 18:01:53 -0300

    edit logo

--- Projeto Integrador 1/Projeto Integrador 1/Properties/Resources.Designer.cs
+++ Projeto Integrador 1/Projeto Integrador 1/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Projeto_Integrador_1.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap logo {
             get {
                 object obj = ResourceManager.GetObject("logo", resourceCulture);
